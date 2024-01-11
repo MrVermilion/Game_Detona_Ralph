208e374b44ad1fb16badb9703e633782e7e4c6d0
MrVermilion <148631658+MrVermilion@users.noreply.github.com>
2024-01-11 18:29:14 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,64 +4,74 @@ const state = {
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score"),
-
     },
     values: {
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
         currentTime: 60,
+        lives: 3, // Propriedade vidas
     },
     actions: {
         timerId: setInterval(randomSquare, 1000),
         countDownTimerId: setInterval(countDown, 1000),
     },
-    };
-    
-    function countDown(){
-        state.values.currentTime--;
-        state.view.timeLeft.textContent = state.values.currentTime;
+};
 
-        if(state.values.currentTime <=0) {
-            clearInterval(state.actions.countDownTimerId)
-            clearInterval(state.actions.timerId)
-            alert("Game Over! Seu resultado foi: " + state.values.result);
-        }
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0 || state.values.lives <= 0) { //Lives incluídas no IF
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! Seu resultado foi: " + state.values.result);
     }
+}
 
-    function playSound(audioName){
-        let audio = new Audio(`./src/audios/${audioName}.m4a`);
-        audio.volume = 0.2;
-        audio.play();
-    }
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
 
-    function randomSquare() {
-        state.view.squares.forEach((square) => {
-            square.classList.remove("enemy");
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            } else {
+                // Clique no quadrado errado
+                state.values.lives--;
+                updateLivesDisplay();
+                playSound("wrong");
+            }
         });
+    });
+}
 
-        let randomNumber = Math.floor(Math.random() * 9);
-        let randomSquare = state.view.squares[randomNumber];
-        randomSquare.classList.add("enemy");
-        state.values.hitPosition = randomSquare.id;
-    }
+function updateLivesDisplay() {
+    const livesDisplay = document.querySelector(".menu-lives h2");
+    livesDisplay.textContent = `x${state.values.lives}`;
+}
 
-    
-    function addListenerHitBox(){
-        state.view.squares.forEach((square) => {
-            square.addEventListener("mousedown", () => {
-                if(square.id === state.values.hitPosition){
-                    state.values.result++;
-                    state.view.score.textContent = state.values.result;
-                    state.values.hitPosition = null;
-                    playSound("hit");
-                }
-            });
-        });
-    }
+function initialize() {
+    addListenerHitBox();
+    updateLivesDisplay();
+}
 
-    function initialize() {
-        addListenerHitBox();
-    }
-
-    initialize();
+initialize();
